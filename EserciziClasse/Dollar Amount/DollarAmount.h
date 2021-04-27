7fdf3dfcba628104cc83f8768df12cfebe5a20f4
TheRealSalvo <salva0729@gmail.com>
2021-04-28 00:11:14 +0200

    Copiato esercizio DollarAmount

---
+++ EserciziClasse/Dollar Amount/DollarAmount.h
@@ -0,0 +1,27 @@
+//DollarAmount.h
+#include <string>
+#include <cmath>
+
+class DollarAmount {
+public:
+	explicit DollarAmount(int64_t value) : amount{ value } {}
+	void add(DollarAmount right) {
+		amount += right.amount;
+	}
+	void subtract(DollarAmount right) {
+		amount -= right.amount;
+	}
+	void addInterst(int rate, int divisor) {
+		DollarAmount interest{
+			(amount * rate + divisor / 2) / divisor;
+		}
+		add(interest);
+	}
+	std::string toString() const {
+		std::string dollars{ std::to_string(amount / 100) };
+		std::string cents{ std::to_string(std::abs(amount % 100)) };
+		return dollars + "." + (cents.size() == 1 ? "0" : "") + cents;
+	}
+private:
+	int64_t amount{ 0 };
+};
