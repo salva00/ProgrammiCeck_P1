230d82a7985507c178b268f8eb23181e68438a9d
Francesco <francescoaiello2000@libero.it>
2021-04-28 21:44:01 +0200

    Aggiunta funzione per convertire anche da binario a decimale

--- EserciziClasse/Dec2Bin/main.cpp
+++ EserciziClasse/Dec2Bin/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 std::string recDec2Bin(int number){
     if(number==0){
@@ -6,13 +7,31 @@ std::string recDec2Bin(int number){
     } else{
         return recDec2Bin(number/2) + std::to_string(number%2);
     }
-};
+}
 
+int raisePow(int base, int exponent) {
+  if(exponent < 0) return 0;
+  int res{1};
+  for(int i = 0; i < exponent; i++) res *= base;
+  return res;
+}
+
+int Bin2Dec(std::string number) {
+  int res{0};
+  for(size_t i = 0; i < number.length(); i++) {
+    if(number[i] == '0') continue;
+    if(number[i] == '1') res += raisePow(2,number.length()-i-1);
+    else throw std::invalid_argument("Number must only contain 0 and 1");
+  }
+  return res;
+}
 
 int main() {
     int number;
     std::cout << "Enter a number: " << std::endl;
     std::cin>>number;
-    std::cout << "Binary: " << recDec2Bin(number) << std::endl;
+    std::string bin = recDec2Bin(number);
+    std::cout << "Binary: " << bin << std::endl;
+    std::cout << "Converted back is: " << Bin2Dec(bin) << std::endl;
     return 0;
 }
