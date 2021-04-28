e626cd95e97b526caf88492c3dc860727095fa52
TheRealSalvo <salva0729@gmail.com>
2021-04-28 17:36:40 +0200

    aggiunto using std::cout

---
+++ EserciziClasse/AccountClass with Constructor/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Account.h"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    // create two Account objects
+    Account account1{ "Jane Green" };
+    Account account2{ "John Blue" };
+
+    // display initial value of name for each Account
+    cout << "account1 name is: " << account1.getName() << endl;
+    cout << "account2 name is: " << account2.getName() << endl;
+    return 0;
+}
