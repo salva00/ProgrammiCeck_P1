1d69bb8165c762ef4b1ca4cb6868a9b92ec955de
Salvatore Sramante <salvatore.bramante@yahoo.it>
2021-04-28 12:15:15 +0200

    Aggiunto file lezione 10

---
+++ EserciziClasse/opefiple1.cpp/main.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+using std::ofstream;
+using std::ios;
+
+int main() {
+    ofstream outClientFile{"clients.txt",ios::out};
+
+    if(!outClientFile){
+        std::cerr<<"File could not be opended"<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::cout<<"Enter the account, name, and balance\n"
+        <<"enter e-o-f to end input.txt ";
+    int account;
+    std::string name;
+    double balance;
+
+    while(std::cin >>account>>name>>balance){
+        outClientFile<<account<<" "<< name <<" " << balance <<std::endl;
+        std::cout<<"? ";
+    }
+
+    return 0;
+}
