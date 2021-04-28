e2677a0f7aee0de00d33f4be3ea051bb6d881539
Francesco <francescoaiello2000@libero.it>
2021-04-28 17:56:02 +0200

    creato .h per la classe matrix

--- EserciziClasse/Matrix/main.cpp
+++ EserciziClasse/Matrix/main.cpp
@@ -1,29 +1,35 @@
-#include <iostream>
-#include "matrix.cpp"
-#include <ctime>
+#include<iostream>
+#include"matrix.h"
+#include<ctime>
 using std::endl;
 using std::cout;
 
 /**************************
 Given a 2D array A mxn and a 2D array B nxk:
 • Compute the matrix product AxB
+• Implement a Matrix class that stores a nxm matrix of double
+• Overload sum (+), difference(-) and product(*) operators;
 **************************/
 
 
 
 int main() {
-  Matrix<double> mat1{{{0,0,-1,1},
-                    {0,0,1,3},
-                    {0,0,5,4},
-                    {0,1,1,1}}};
-  // Matrix<double> mat1(8,8);
-  // mat1.random(1,time(0),-1);
+  Matrix<double> mat1{{{1,0,0},
+                      {0,1,0},
+                      {0,0,1}}};
+
+  cout << "top left is " << mat1.at(0,0);
+  cout << "performing \n";
   mat1.print();
-  cout << "\n";
-  Matrix<double> mat2 = mat1.gauss();
-  cout << "\n";
+  cout << "+\n";
+  Matrix<double> mat2{3,3,1};
   mat2.print();
-  cout << "\nDeterminant is " << mat2.diag() << "\n\n";
+  cout << "=\n";
+  Matrix<double> m3 = mat1+mat2;
+  m3.print();
+
+
+
 
   system("pause");
   return 0;
--- EserciziClasse/Matrix/matrix.cpp
+++ EserciziClasse/Matrix/matrix.cpp
@@ -1,159 +1,192 @@
 #include <iostream>
-#include <vector>
 #include <iomanip>
 #include <cstdlib>
+#include"matrix.h"
 using std::vector;
 
+
 template<typename T>
-class Matrix {
-public:
-  // costructors
-  Matrix(size_t r, size_t c, T to_fill = 0) : rows(r),
-    cols(c), elements(r*c,to_fill) {}
-  Matrix(const vector<vector<T>>& input) : rows(input.size()), cols(input[0].size()) {
-    for(size_t i = 0; i < input.size(); i++) {
-      for(size_t j = 0; j < input[0].size(); j++) {
-        this->elements.push_back(input[i][j]);
-      }
+//construct r x c matrix filled with to_fill
+Matrix<T>::Matrix(size_t r, size_t c, T to_fill) : rows(r),
+  cols(c), elements(r*c,to_fill) {}
+
+template<typename T>
+Matrix<T>::Matrix(const vector<vector<T>>& input) : rows(input.size()), cols(input[0].size()) {
+  for(size_t i = 0; i < input.size(); i++) {
+    for(size_t j = 0; j < input[0].size(); j++) {
+      this->elements.push_back(input[i][j]);
     }
   }
-  // getter-setters
-  T at(size_t row, size_t col) { // to access elements
+}
+
+template<typename T>
+//safe access elements
+T Matrix<T>::at(size_t row, size_t col) const {
     return this->elements.at(row * this->cols + col);
   }
-  const size_t& getRows() const {return this->rows;}
-  const size_t& getCols() const {return this->cols;}
-  void put(T what, size_t row, size_t col) {
+
+template<typename T>
+const size_t Matrix<T>::getRows() const {return this->rows;}
+
+template<typename T>
+const size_t Matrix<T>::getCols() const {return this->cols;}
+
+template<typename T>
+//change elements
+void Matrix<T>::put(T what, size_t row, size_t col) {
     this->elements[(row * this->cols) + col] = what;
-  }
+}
 
-  void random(int max, unsigned int seed = 0, int min = 0) {
-    srand(seed);
-    if(max < 0 || min > max) return;
-    for(size_t i = 0; i < this->rows; i++) {
-      for(size_t j = 0; j < this->cols; j++) {
-        elements[(i*this->cols)+j] = (rand() % (max-min+1)) + min;
-      }
+template<typename T>
+//randomise matrix
+void Matrix<T>::random(int max, unsigned int seed, int min) {
+  srand(seed);
+  if(max < 0 || min > max) return;
+  for(size_t i = 0; i < this->rows; i++) {
+    for(size_t j = 0; j < this->cols; j++) {
+      elements[(i*this->cols)+j] = (rand() % (max-min+1)) + min;
     }
   }
+}
 
-  void print() const {
-    for(size_t i = 0; i < this->rows; i++) {
-      for(size_t j = 0; j < this->cols; j++) {
-        std::cout << std::setw(4) << elements[(i*this->cols)+j] << " ";
-      }
-      std::cout << std::endl;
+template<typename T>
+//print matrix
+void Matrix<T>::print() const {
+  for(size_t i = 0; i < this->rows; i++) {
+    for(size_t j = 0; j < this->cols; j++) {
+      std::cout << std::setw(4) << elements[(i*this->cols)+j] << " ";
     }
-    return;
+    std::cout << '\n';
   }
+  return;
+}
 
-  Matrix<T> operator*(Matrix<T> mult) {
-    // takes m*n and n*p , returns m*p
-    // this * mult
-    if(mult.getRows() != this->cols) throw;
+template<typename T>
+//executes dot product
+Matrix<T> Matrix<T>::operator*(Matrix<T> mult) {
+  // takes m*n and n*p , returns m*p
+  // this * mult
+  if(mult.getRows() != this->cols)
+    throw std::invalid_argument("Left operand rows must match second operand columns");
+  size_t m = this->rows, n = this->cols;
+  size_t p = mult.getCols();
+  int temp{0};
+  Matrix<T> res(m,p);
+  for(size_t i = 0; i < m; i++) {
+    for(size_t j = 0; j < p; j++) {
+      for(size_t k = 0; k < n; k++) {
+        temp += (this->elements[i*n + k] * mult.at(k,j));
+        // std::cout << "(i,j) is " << i << " " << j << " ~ k is " << k << " ~~~";
+        // std::cout << "doing " << this->elements[i*n + k]
+        // << " * " <<  mult.at(k,j) << "\n";
+      }
+      res.put(temp,i,j);
+      temp = 0;
+    }
+  }
+  return res;
+}
 
-    size_t m = this->rows, n = this->cols;
-    size_t p = mult.getCols();
-    int temp{0};
-    Matrix<T> res(m,p);
-    for(size_t i = 0; i < m; i++) {
-      for(size_t j = 0; j < p; j++) {
-        for(size_t k = 0; k < n; k++) {
-          temp += (this->elements[i*n + k] * mult.at(k,j));
-          // std::cout << "(i,j) is " << i << " " << j << " ~ k is " << k << " ~~~";
-          // std::cout << "doing " << this->elements[i*n + k]
-          // << " * " <<  mult.at(k,j) << "\n";
+template<typename T>
+//executes scalar product
+Matrix<T> Matrix<T>::operator*(T mult) {
+  Matrix<T> res = *this;
+  for(size_t i = 0; i < this->rows; i++) {
+    for(size_t j = 0; j < this->cols; j++) {
+      res.put(this->elements[(i*this->cols)+j] * mult,i,j);
+    }
+  }
+  return res;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T>& rhs) const {
+  if(this->rows != rhs.rows || this->cols != rhs.cols)
+    throw std::invalid_argument("Matrix dimensions does not match");
+  Matrix<T> res = *this;
+  for(size_t i = 0; i < this->rows; i++) {
+    for(size_t j = 0; j < this->cols; j++) {
+      res.put(this->at(i,j) + rhs.at(i,j),i,j);
+    }
+  }
+  return res;
+}
+
+template<typename T>
+//swap column a and b
+void Matrix<T>::colSwap(size_t a, size_t b) {
+  T temp;  // a>>temp b>>a temp>>b
+  for(size_t i = 0; i < this->rows; i++) {
+    temp = this->at(i,a);
+    put(this->at(i,b),i,a);
+    put(temp,i,b);
+  }
+}
+
+template<typename T>
+//swap row a and b
+void Matrix<T>::rowSwap(size_t a, size_t b) {
+  T temp;  // a>>temp b>>a temp>>b
+  for(size_t i = 0; i < this->cols; i++) {
+    temp = this->at(a,i);
+    put(this->at(b,i),a,i);
+    put(temp,b,i);
+  }
+}
+
+template<typename T>
+//returns the gauss-reduced version of the matrix
+Matrix<T> Matrix<T>::gauss() const {
+  Matrix<T> res = *this;
+  size_t n = (res.getCols() < res.getRows()) ?
+    res.getCols() : res.getRows();
+    T r;
+  for(size_t i = 0; i < n; i++) {
+    try {
+      for(size_t l = i+1; res.at(i,i) == 0 && l < res.getRows(); l++) {
+        // std::cout << "checking is (" << l << "," << i << ") = " << res.at(i,l) << "is 0\n";
+        if(res.at(l,i) != 0) {
+          res.rowSwap(i,l);
+          res.inverted = !(res.inverted);
         }
-        res.put(temp,i,j);
-        temp = 0;
+      }
+    } catch(...) {std::cout << "yeah";}
+    // swap if 0
+    for(size_t j = i+1; j < res.getRows(); j++) {
+      //computes R = (j..,i) / (i,i)  and multiples
+      // every () by R*(i,k)
+      if(res.at(j,i) == 0) continue;
+      res.print();
+      system("pause");
+      // std::cout << "i = " << i << " ~ j = " << j << "\n"
+      // << "r = " << res.at(j,i) << "/" << res.at(i,i) << " = " << res.at(j,i)/res.at(i,i) << "\n";
+      r = res.at(j,i)/res.at(i,i);
+      for(size_t k = i; k < res.getCols(); k++) { // reduce rows
+        // for every element in row, takes the element at raw i (same col),
+        // multiplies it by r, which is subtracted
+        // std::cout << "i = " << i << " ~ j = " << j << "~ k = " << k << "\n"
+        // << res.at(j,k) << " is subtracted by " << res.at(i,k)*r << "\n";
+        //this->elements[(j * this->cols) + k] -= this->at(i,k) * r;
+        res.put((res.at(j,k)-res.at(i,k)*r),j,k);
       }
     }
-    return res;
   }
+  return res;
+}
 
-  Matrix<T> operator*(T mult) {
-    Matrix<T> res = *this;
-    for(size_t i = 0; i < this->rows; i++) {
-      for(size_t j = 0; j < this->cols; j++) {
-        res.put(this->elements[(i*this->cols)+j] * mult,i,j);
-      }
-    }
-    return res;
-  }
-
-  void colSwap(size_t a, size_t b) {
-    T temp;  // a>>temp b>>a temp>>b
-    for(size_t i = 0; i < this->rows; i++) {
-      temp = this->at(i,a);
-      put(this->at(i,b),i,a);
-      put(temp,i,b);
-    }
-  }
-  void rowSwap(size_t a, size_t b) {
-    T temp;  // a>>temp b>>a temp>>b
-    for(size_t i = 0; i < this->cols; i++) {
-      temp = this->at(a,i);
-      put(this->at(b,i),a,i);
-      put(temp,b,i);
-    }
-  }
-
-  Matrix<T> gauss() const {
-    Matrix<T> res = *this;
-    size_t n = (res.getCols() < res.getRows()) ?
-      res.getCols() : res.getRows();
-      T r;
+template<typename T>
+//returns the determinant of a diagonal matrix
+T Matrix<T>::diag() const {
+  size_t n = (this->cols < this->rows) ? this->cols : this->rows;
+  T res{1};
     for(size_t i = 0; i < n; i++) {
-      try {
-        for(size_t l = i+1; res.at(i,i) == 0 && l < res.getRows(); l++) {
-          // std::cout << "checking is (" << l << "," << i << ") = " << res.at(i,l) << "is 0\n";
-          if(res.at(l,i) != 0) {
-            res.rowSwap(i,l);
-            res.inverted = !(res.inverted);
-          }
-        }
-      } catch(...) {std::cout << "yeah";}
-      // swap if 0
-      for(size_t j = i+1; j < res.getRows(); j++) {
-        //computes R = (j..,i) / (i,i)  and multiples
-        // every () by R*(i,k)
-        if(res.at(j,i) == 0) continue;
-        res.print();
-        system("pause");
-        // std::cout << "i = " << i << " ~ j = " << j << "\n"
-        // << "r = " << res.at(j,i) << "/" << res.at(i,i) << " = " << res.at(j,i)/res.at(i,i) << "\n";
-        r = res.at(j,i)/res.at(i,i);
-        for(size_t k = i; k < res.getCols(); k++) { // reduce rows
-          // for every element in row, takes the element at raw i (same col),
-          // multiplies it by r, which is subtracted
-          // std::cout << "i = " << i << " ~ j = " << j << "~ k = " << k << "\n"
-          // << res.at(j,k) << " is subtracted by " << res.at(i,k)*r << "\n";
-          //this->elements[(j * this->cols) + k] -= this->at(i,k) * r;
-          res.put((res.at(j,k)-res.at(i,k)*r),j,k);
-        }
-
-      }
+      res *= this->at(i,i);
     }
-    return res;
-  }
+    if(this->inverted) {
+      return -res;
+    } else return res;
+}
 
-  T diag() const {
-    size_t n = (this->cols < this->rows) ? cols : rows;
-    T res{1};
-      for(size_t i = 0; i < n; i++) {
-        res *= this->at(i,i);
-      }
-      if(this->inverted) {
-        return -res;
-      } else return res;
-  }
-
-private:
-  const size_t rows;
-  const size_t cols;
-  std::vector<T> elements;
-  bool inverted{0};
-};
 
 
 //
