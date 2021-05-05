56493ba91acda2e10937201e837c8172536eef50
Salvatore Bramante <salvatore.bramante@yahoo.it>
2021-05-05 22:32:40 +0200

    Check Makefile

---
+++ EserciziClasse/CIrcularlyLinkedList/Cnode.h
@@ -0,0 +1,19 @@
+//
+// Created by Salvatore Bramante on 05/05/21.
+//
+
+#ifndef CIRCULARLYLINKEDLIST_CNODE_H
+#define CIRCULARLYLINKEDLIST_CNODE_H
+
+template<typename T> class CircularlyLinkedList;
+
+template <typename T>
+class Cnode {
+private:
+    T element;
+    Cnode* next;
+    friend class CircularlyLinkedList<T>;
+};
+
+
+#endif //CIRCULARLYLINKEDLIST_CNODE_H
