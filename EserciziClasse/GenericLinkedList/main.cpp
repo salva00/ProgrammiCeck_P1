0ff586ceda5d7934a2571d05816dcc4632c312dc
Salvatore Sramante <salvatore.bramante@yahoo.it>
2021-05-05 10:37:54 +0200

    Updated Make and main

--- EserciziClasse/GenericLinkedList/main.cpp
+++ EserciziClasse/GenericLinkedList/main.cpp
@@ -2,15 +2,20 @@
 #include "LinkedList.h"
 #include <string.h>
 
+template <typename T>
+
+void printFront(LinkedList<T> &list){
+    std::cout<<list.front()<<std::endl;
+}
 
 int main() {
     LinkedList<std::string> L;
     L.addFront("kd");
     L.addFront("JK");
-    std::cout << L.front() << std::endl;
+    printFront(L);
     LinkedList<int> L2;
     L2.addFront(1);
     L2.addFront(3);
-    std::cout << L2.front() << std::endl;
+    printFront(L2);
     return 0;
 }
