c8bb814ce5a13a01314c975e0ff9e363396f7b07
Salvatore Bramante <salvatore.bramante@yahoo.it>
2021-06-05 00:00:56 +0200

    Utile all'esame parte 2

--- EserciziClasse/HeapAlgorithms/main.cpp
+++ EserciziClasse/HeapAlgorithms/main.cpp
@@ -18,10 +18,16 @@ void addToHeap(std::vector<TYPE>* collection, const TYPE& elem){
     std::push_heap(collection->begin(),collection->end());
 }
 
+template<typename TYPE>
+void sortHeap(std::vector<TYPE>* collection){   //ordine crescente
+    std::sort_heap(collection->begin(),collection->end());
+}
+
 int main(){
     std::vector<int> po {0,3,4,7,6,8,5};
     heapfy(&po);
     addToHeap(&po,69);      //funny number ;)
+    sortHeap(&po);
     //std::cout << maxElement(&po) << std::endl;
     return 0;
 }
