f764e7b634e3bd3e3f333b17c46fb2469f14cd17
Francesco <francescoaiello2000@libero.it>
2021-06-08 22:18:26 +0200

    Migliorato bubble sort

--- EserciziClasse/lib/BubbleSort.h
+++ EserciziClasse/lib/BubbleSort.h
@@ -26,22 +26,10 @@ void bubble_sort(Iterator begin, Iterator end);
 
 template<class Iterator>
 void mysort::bubble_sort(Iterator begin, Iterator end) {
-	auto b = begin, e = end;
-	for (Iterator sorted = begin; begin != end; end = sorted ) {
-		sorted = begin;
-		for (Iterator current = begin, prev = begin; ++current != end; ++prev) {
-			// mysort::print(b,e);
-			if(*current < *prev) {
-				std::iter_swap(current, prev);
-				// std::cout << "swap!\n";
-				// mysort::print(b,e);
-				sorted = current;
-			}
-		}
-	}
+	for (Iterator i = begin; i != end; ++i)
+			for (Iterator j = begin; j < i; ++j)
+					if (*i < *j) std::iter_swap(i, j);
 }
-
-
 
 
 #endif
