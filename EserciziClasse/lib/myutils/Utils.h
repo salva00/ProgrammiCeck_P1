1599a3b33ed3bc01f0b83c9a0bef4bff3344e934
Salvatore Bramante <salvatore.bramante@yahoo.it>
2021-06-11 10:21:40 +0200

    Added deserialization in string type

--- EserciziClasse/lib/myutils/Utils.h
+++ EserciziClasse/lib/myutils/Utils.h
@@ -1,96 +1,70 @@
+//
+// Created by Salvatore Bramante on 10/06/21.
+//
+
 #ifndef UTILS_H
 #define UTILS_H
-
 #include <fstream>
-#include <iostream>
 #include <utility>
 #include <vector>
-#include "Types.h"
+#include "Helpers.h"
 
 namespace myutils {
-	using std::ostream;
-	using std::string;
-	using std::ofstream;
-	using std::ifstream;
-	using std::ios;
-	using std::vector;
+    using std::ostream;
+    using std::string;
+    using std::ofstream;
+    using std::ifstream;
+    using std::ios;
+    using std::vector;
 
-	struct Instance {
-		std::string name;
-		Type* value;
-	};
 
-	template <typename T>
-	void seq_write(const T& data, string path){
-		ofstream file("filename.txt", std::ios::out);
-		if (file.is_open()) {
-			file << data <<"\n";
-		} else {
-			throw std::invalid_argument("Cannot open file");
-		}
-		file.close();
-	}
 
-	vector<Instance> read(vector<std::pair<string,Types>> arguments){
-		ifstream file("filename.txt", std::ios::in);
-		vector<Instance> result;
-		if (file.is_open()) {
-			while (!file.eof()) {
-				for (auto arg: arguments) {
-					Instance elem;
-					// switch (arg.second) {
-					// 	case Types::STRING:
-					// 		string temp;
-					// 		std::getline(file, temp);
-					// 		//dynamic_cast<String>((elem.value).setValue(temp)); BOH
-					// 		// VECTOR <3 :)
-					// 		break;
-					// 	case Types::INT:
-					// 		int pippo;
-					// 		file >> pippo;
-					// 		elem.value->setValue(pippo);
-					// 	case Types::FLOAT:
-					// 		float pippo1;
-					// 		file >> pippo;
-					// 		elem.value->setValue(pippo);
-					// 	case Types::DOUBLE:
-					// 		double pippo2;
-					// 		file >> pippo;
-					// 		elem.value->setValue(pippo);
-					// 	case Types::BOOL:
-					// 		bool pippo;
-					// 		file >> pippo3;
-					// 		elem.value->setValue(pippo);
-					// 	case Types::CHAR:
-					// 		char pippo4;
-					// 		file >> pippo;
-					// 		elem.value->setValue(pippo);
-					// 	default: throw std::invalid_argument("Sei scemo");
-					// }
-					// result.push_back(elem);
-				}
-				//file >> res;
-			}
-		} else {
-			throw std::invalid_argument("Cannot open file");
-		}
-		file.close();
-		return result;
-	}
+    template <typename T>
+    void seq_write(const T& data, const string& path){
+        ofstream file(path, std::ios::out);
+        if (file.is_open()) {
+            file << data <<"\n";
+        } else {
+            throw std::invalid_argument("Cannot open file");
+        }
+        file.close();
+    }
+
+
+    template <typename T>
+    void bin_init(T blank, int numberOfElements, const string& path){
+        ofstream file(path, std::ios::binary | std::ios::out | std::ios::app);
+        if (file.is_open()) {
+            for (int i=0; i<numberOfElements; i++){
+                file.write(reinterpret_cast<const char*>(blank), sizeof(T));
+            }
+        } else {
+            throw std::invalid_argument("Cannot open file");
+        }
+        file.close();
+    }
+
+
+    vector<Instance> seq_deserialize(const vector<std::pair<string,Types>>& arguments){
+        ifstream file("filename.txt", std::ios::in);
+        vector<Instance> result;
+        if (file.is_open()) {
+            while (!file.eof()) {
+                for (auto arg: arguments) {
+                    result.push_back(getCorrectType(file,arg));
+                    //result.front().value->getType();
+                }
+                //file >> res;
+            }
+        } else {
+            throw std::invalid_argument("Cannot open file");
+        }
+        file.close();
+        return result;
+    }
 
 
 
 } /* myutils */
 
-#endif
-
-
-
-
-
-
-
-
-
-
-//
+#endif //SORTHEAP_UTILS_H
